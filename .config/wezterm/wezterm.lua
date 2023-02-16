674174809177a50f33dbe22bd45185eaf29133e9
takaoyuri <1169171+takaoyuri@users.noreply.github.com>
2023-02-16 18:32:58 +0900

    add wezterm.lua

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wez = require 'wezterm'
+
+-- フォント名
+local font_name = "UDEV Gothic 35NFLG"
+
+-- タブの横幅を調整したい
+wez.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local fmt = {
+    { Text = '  ' .. tab.active_pane.title .. '  ' },
+  }
+  return fmt
+end
+)
+
+return {
+    font = wez.font(font_name),
+    -- font = wez.font {
+    --    family = 'Cica',
+    --    weight = 'Bold',
+    -- },
+    font_size = 18,
+    use_ime = true,
+    enable_scroll_bar = true,
+    mi_scroll_bar_height = '2cell',
+    window_background_opacity = 0.9,
+    text_background_opacity = 0.5,
+    window_frame = {
+      font = wez.font(font_name),
+      font_size = 18
+    }
+}
