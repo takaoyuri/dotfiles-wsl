78213447480bd6a70c8675c051c1649b531a76b4
takaoyuri <1169171+takaoyuri@users.noreply.github.com>
2024-12-09 11:14:25 +0900

    update

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -3,7 +3,9 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+focus_sid=$(aerospace list-workspaces --focused)
+
+if [ "$1" = "$focus_sid" ]; then
     sketchybar --set $NAME background.drawing=on
 else
     sketchybar --set $NAME background.drawing=off
