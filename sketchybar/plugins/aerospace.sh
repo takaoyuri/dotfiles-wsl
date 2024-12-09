f590ea40c223c25b52ab5f7a4be98f83ebb0bf44
takaoyuri <1169171+takaoyuri@users.noreply.github.com>
2024-12-09 13:50:53 +0900

    update

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 focus_sid=$(aerospace list-workspaces --focused)
-apps=$(aerospace list-windows --workspace $focus_sid --format '%{app-name} ')
+# apps=$(aerospace list-windows --workspace $focus_sid --format '%{app-name} ')
 
 if [ "$1" = "$focus_sid" ]; then
     sketchybar --set $NAME background.drawing=on
@@ -12,4 +12,7 @@ else
     sketchybar --set $NAME background.drawing=off
 fi
 
-sketchybar --set space.$focus_sid label="$focus_sid $apps" 
+for sid in $(aerospace list-workspaces --all); do
+    apps=$(aerospace list-windows --workspace $sid --format '%{app-name} ')
+    sketchybar --set space.$sid label="$sid $apps" 
+done
